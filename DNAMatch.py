0c75a5efaec92c6617c5ba576b3af96e21472802
andrewlee <drweleean@gmail.com>
2023-01-26 10:57:38 -0700

    first attempt

---
+++ DNAMatch.py
@@ -0,0 +1,44 @@
+def dna_match_bottomup(DNA1, DNA2):
+    m = len(DNA1)
+    n = len(DNA2)
+    cache = [[0 for x in range(n+1)] for x in range(m+1)]
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                cache[i][j] = 0
+            elif str1[i - 1] == str2[j - 1]:
+                cache[i][j] = cache[i - 1][j - 1] + 1
+            else:
+                cache[i][j] = max(cache[i - 1][j], cache[i][j - 1])
+    return cache[m][n]
+
+def dna_match_topdown_helper(DNA1, DNA2, m, n, cache):
+    if m < 0 or n < 0:
+        return 0
+    elif DNA1[m] == DNA2[n]:
+        if cache[m-1][n-1] > 0:
+            temporary_result = 1 + cache[m-1][n-1]
+        else:
+            temporary_result = 1 + dna_match_topdown_helper(DNA1, DNA2, m-1, n-1, cache)
+    else:
+        if cache[m-1][n] > 0 and cache[m][n-1] > 0:
+            temporary_result = max(cache[m-1][n], cache[m][n-1])
+        elif cache[m-1][n] > 0:
+            temporary_result = max(cache[m-1][n], dna_match_topdown_helper(DNA1, DNA2, m, n-1, cache))
+        elif cache[m][n-1] > 0:
+            temporary_result = max(dna_match_topdown_helper(DNA1, DNA2, m-1, n, cache), cache[m][n-1])
+        else:
+            temporary_result = max(dna_match_topdown_helper(DNA1, DNA2, m-1, n, cache), dna_match_topdown_helper(DNA1, DNA2, m, n-1, cache))
+
+    if cache[m][n] < temporary_result:
+        cache[m][n] = temporary_result
+
+    return cache[m][n]
+
+def dna_match_topdown(DNA1, DNA2):
+    m = len(DNA1)-1
+    n = len(DNA2)-1
+    if m < 0 or n< 0:
+        return 0
+    cache = [[0 for x in range(n+1)] for x in range(m+1)]
+    return dna_match_topdown_helper(DNA1, DNA1, m, n, cache)
