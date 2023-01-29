e7cf9dd25579b48b9f90d8b4c44275d67bedcda8
andrewlee <drweleean@gmail.com>
2023-01-28 20:13:07 -0700

    added comments

--- DNAMatch.py
+++ DNAMatch.py
@@ -1,8 +1,17 @@
+#Andrew Lee
+#CS 325
+#Assignment 3
+#Due Date: January 30, 2023
+
+
+
 def dna_match_bottomup(DNA1, DNA2):
-    m = len(DNA1)
+    #function that takes two given DNA and returns the largest substring, using a bottom up approach
+    m = len(DNA1)                           #var names for the DNA strings
     n = len(DNA2)
-    cache = [[0 for x in range(n+1)] for x in range(m+1)]
-    for i in range(m + 1):
+    cache = [[0 for x in range(n+1)] for x in range(m+1)]       #initialize our 2D array
+    for i in range(m + 1):                                      #for loop logic from the Exploration
+        #starting from 0, 0 in our 2D array, aka bottom up approach
         for j in range(n + 1):
             if i == 0 or j == 0:
                 cache[i][j] = 0
@@ -10,7 +19,7 @@ def dna_match_bottomup(DNA1, DNA2):
                 cache[i][j] = cache[i - 1][j - 1] + 1
             else:
                 cache[i][j] = max(cache[i - 1][j], cache[i][j - 1])
-    return cache[m][n]
+    return cache[m][n]                                          #return the bottom right value to get our answer
 
 def dna_match_topdown_helper(DNA1, DNA2, m, n, cache):
     if m < 0 or n < 0:
@@ -36,9 +45,11 @@ def dna_match_topdown_helper(DNA1, DNA2, m, n, cache):
     return cache[m][n]
 
 def dna_match_topdown(DNA1, DNA2):
-    m = len(DNA1)-1
-    n = len(DNA2)-1
+
+    m = len(DNA1)#-1
+    n = len(DNA2)#-1
     if m < 0 or n< 0:
         return 0
     cache = [[0 for x in range(n+1)] for x in range(m+1)]
     return dna_match_topdown_helper(DNA1, DNA1, m, n, cache)
+
