195bcb2f05e77a1e78d956911774e6b20b1bab40
andrewlee <drweleean@gmail.com>
2023-01-29 21:11:36 -0700

    added comments

--- DNAMatch.py
+++ DNAMatch.py
@@ -22,14 +22,18 @@ def dna_match_bottomup(DNA1, DNA2):
     return cache[m][n]                                          #return the bottom right value to get our answer
 
 def dna_match_topdown_helper(DNA1, DNA2, m, n, cache):
+    '''helper function that takes our cache and adds values to it as we find our answers. Uses similar logic/recurrence
+    formula from Exploration 3.3'''
     if m <= 0 or n <= 0:
         return 0
     if DNA1[m-1] == DNA2[n-1]:
-        if cache[m-1][n-1] > 0:
+        if cache[m-1][n-1] > 0:                                 #if in cache, then we use it for our calculations
             temporary_result = 1 + cache[m-1][n-1]
         else:
-            temporary_result = 1 + dna_match_topdown_helper(DNA1, DNA2, m-1, n-1, cache)
+            temporary_result = 1 + dna_match_topdown_helper(DNA1, DNA2, m-1, n-1, cache) #else, we have to make a call
     else:
+        #very cases based on whether or not it's in our cache or not. If it's in the cache, then we just use the value
+        #from the cache. Else, we make another recursive call
         if cache[m-1][n] > 0 and cache[m][n-1] > 0:
             temporary_result = max(cache[m-1][n], cache[m][n-1])
         elif cache[m-1][n] > 0:
@@ -45,12 +49,11 @@ def dna_match_topdown_helper(DNA1, DNA2, m, n, cache):
     return cache[m][n]
 
 def dna_match_topdown(DNA1, DNA2):
-
+    '''function that uses a top bottom approach to find the longest common substring between two given DNA strings'''
     m = len(DNA1)
     n = len(DNA2)
     if m <= 0 or n<= 0:
         return 0
-    cache = [[0 for x in range(n+1)] for x in range(m+1)]
+    cache = [[0 for x in range(n+1)] for x in range(m+1)]           #initiate our 2D cache
     return dna_match_topdown_helper(DNA1, DNA2, m, n, cache)
 
-print(dna_match_topdown("ace", "abcdef"))
